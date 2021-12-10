a338ed3b6bdd3c249f0d0be62a300c9799183e2d
Yan <yan@metatem.net>
2021-12-10 22:14:05 +0100

    correct darwin library

--- library.lisp
+++ library.lisp
@@ -1,7 +1,7 @@
 (in-package #:CL-FreeImage)
 
 (cffi:define-foreign-library :libFreeImage
-  (cffi-features:darwin "libpango.dylib")
+  (cffi-features:darwin (:default "libfreeimage"))
   (cffi-features:unix (:or "libfreeimage.so" "libfreeimage.so.3"))
   (cffi-features:windows "libfreeimage.dll"))
 
